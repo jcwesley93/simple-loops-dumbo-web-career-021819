5983af50f72317ba09c8b9810ffae817d3ba7b7a
Jaela Wesley <jcwesley93@gmail.com>
2019-02-02 02:02:05 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -24,7 +24,7 @@ def times_iterator(number_of_times)
   7.times.do
     puts "Welcome to Flation School's Web Development Course!"
   end
-end
+
 
 def while_iterator(number_of_times)
   # code your solution here using the "while" keyword

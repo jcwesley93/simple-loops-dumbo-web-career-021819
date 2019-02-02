22b0e5f80dbc93d7434699e035ae0a57d6da416d
Jaela Wesley <jcwesley93@gmail.com>
2019-02-02 02:33:05 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -20,7 +20,7 @@ end
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   number_of_times.times do
-    puts "Welcome to Flation School's Web Development Course!"
+    puts "Welcome to Flatiron School's Web Development Course!"
 end
 end
 
